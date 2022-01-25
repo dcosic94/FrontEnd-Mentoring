f74c5faa1bc1a51c3615cdbf99a410a1cce04775
Domagoj Ćosić <domagojrawdigital.hr>
2022-01-25 10:18:29 +0100

    Navigation logic

--- nav.js
+++ nav.js
@@ -1,28 +1,42 @@
-const navigation = document.querySelector('.main-navigation-list');
-const hamburger = document.querySelector('.hamburger');
-const close = document.querySelector('.close-btn');
+"use strict";
+// without use strict, an assignment to a non-existing variable creates a new global variable, for compatibility with old code
 
-hamburger.addEventListener('click', function() {
+const navigation = document.querySelector(".main-navigation-list");
+const navigationItems = navigation.querySelectorAll("a");
+const hamby = document.querySelector(".hamburger");
 
-    const expanded = JSON.parse(hamburger.getAttribute('aria-expanded'));
-    hamburger.setAttribute('aria-expanded', !expanded);
+const open = () => {
+  hamby.setAttribute("aria-expanded", "true");
+  navigation.classList.add("is-visible");
 
-    navigation.classList.toggle('is-visable');
-    close.classList.toggle('active');
-    hamburger.classList.toggle('inactive');
+  setTimeout(() => navigation.classList.add("is-active"));
+};
 
+const close = () => {
+  hamby.setAttribute("aria-expanded", "false");
+  navigation.classList.remove("is-active");
+};
 
+const handleClosure = (event) => !navigation.contains(event.target) && close();
 
-    setTimeout(() => {
-        navigation.classList.toggle('is-active');
-    });
+hamby.addEventListener("click", (event) => {
+  event.stopPropagation();
+  // we need to stop the event from bubbling up to the document and firing our click event which would close the navigation
+
+  const expanded = JSON.parse(hamby.getAttribute("aria-expanded"));
+  expanded ? close() : open();
 });
 
-close.addEventListener('click', function() {
-    close.classList.remove('active');
-    hamburger.classList.remove('inactive');
-    hamburger.classList.toggle('active');
-    navigation.classList.toggle('is-active');
-    navigation.classList.toggle('is-visable');
+navigation.addEventListener("transitionend", (event) => {
+  event.target === navigation &&
+    // since we have a transition on .Contact button, that event also bubbles up to this handler so we need to check if the element is navigation itself
+    !event.target.classList.contains("is-active") &&
+    // we need to remove the visible class only if the navigation is fully open, so we check for a is-active class
+    navigation.classList.remove("is-visible");
 });
 
+document.addEventListener("click", handleClosure);
+document.addEventListener("focusin", handleClosure);
+
+navigationItems.forEach((link) => link.addEventListener("click", () => close())
+);
